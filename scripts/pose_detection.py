8e94b12647d500243733660698d2536939ca8ee4
Chong Jun Yi <junyichong@gmail.com>
2023-05-26 12:10:27 +0900

    Write csv

--- scripts/pose_detection.py
+++ scripts/pose_detection.py
@@ -1,8 +1,23 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import csv
+import os
 
 
+# Get coordinates of body part
+def get_coordinates(landmarks, body_part):
+    if hasattr(mp_pose.PoseLandmark, body_part):
+        body_part_index = mp_pose.PoseLandmark[body_part].value
+        x = landmarks[body_part_index].x
+        y = landmarks[body_part_index].y
+        z = landmarks[body_part_index].z
+        return [x, y, z]
+    else:
+        return None
+
+
+# Calculate angle between three points
 def calculate_angle(a,b,c):
     a = np.array(a) # First
     b = np.array(b) # Mid
@@ -15,26 +30,25 @@ def calculate_angle(a,b,c):
         angle = 360-angle
         
     return angle
-    
-
-def get_coordinates(landmarks, body_part):
-    if hasattr(mp_pose.PoseLandmark, body_part):
-        body_part_index = mp_pose.PoseLandmark[body_part].value
-        x = landmarks[body_part_index].x
-        y = landmarks[body_part_index].y
-        return [x, y]
-    else:
-        return None
 
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-video_path = 'test_videos/type2.mp4'
+video_path = 'test_videos/type3.mp4'
 cap = cv2.VideoCapture(video_path)
 
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+folder_path = os.path.join(os.getcwd(), 'results')
+os.makedirs(folder_path, exist_ok=True)
+csv_path = os.path.join(folder_path, 'type3.csv')
+
+with open(csv_path, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['left angle', 'right angle'])
+
 
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
@@ -60,9 +74,13 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             left_elbow = get_coordinates(landmarks, 'LEFT_ELBOW')
             left_wrist = get_coordinates(landmarks, 'LEFT_WRIST')
 
+            print(left_shoulder)
+
             right_shoulder = get_coordinates(landmarks, 'RIGHT_SHOULDER')
             right_elbow = get_coordinates(landmarks, 'RIGHT_ELBOW')
             right_wrist = get_coordinates(landmarks, 'RIGHT_WRIST')
+
+            print(right_shoulder)
 
             # Calculate angle
             left_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
@@ -77,10 +95,15 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                            tuple(np.multiply(right_elbow, [frame_width, frame_height]).astype(int)), 
                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA
                                 )
-                       
+
         except:
             pass
         
+        # Write results to .csv file
+        with open(csv_path, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([left_angle, right_angle])
+
         # Render detections
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
